5d605a2f76dc83336948db2e09d70d3e7fe63b02
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2018-11-20 16:32:10 +0900

    20/11 Upload Test

--- WEB-INF/classes/Upload.java
+++ WEB-INF/classes/Upload.java
@@ -1,41 +1,17 @@
 import java.io.IOException;
-import java.io.File;
-import java.io.InputStream;
-import java.io.FileOutputStream;
 
 import javax.servlet.http.Part;
 
 public class Upload{
-	final byte[] buff=new byte[4*1024];
-	final String url = "C:/LittlePlayground/uploadTest/image/";
-	
-	InputStream input;
-	FileOutputStream output;
-	int read = 0;
+	final String url = "C:/github/Animal-Snap/post-url/";
 	DAO dao = new DAO();
 	
 	public void uploadPhoto(Part part, String caption){
 		String fileName = getFileName();
 		try{
-			input = part.getInputStream();
-			File file = new File(url+fileName);
-			output = new FileOutputStream(file);
-			while((read = input.read(buff)) != -1) {
-				output.write(buff, 0, read);
-			}
+			part.write(url+fileName);
 		} catch (IOException e) {
 			e.printStackTrace();
-		} finally {
-			try{
-				if (output != null) {
-					output.close();
-				}
-				if (input != null) {
-					input.close();
-				}
-			}catch(IOException e){
-				e.printStackTrace();
-			}
 		}
 		dao.post(url+fileName, caption);
 	}
