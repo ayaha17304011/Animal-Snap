0561b182db31b07388a31926cbe26d06e053ad8d
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2018-11-28 13:46:26 +0900

    Add ReplyBean

--- WEB-INF/classes/beans/ReplyBean.java
+++ WEB-INF/classes/beans/ReplyBean.java
@@ -1,5 +1,43 @@
 package beans;
 
 public class ReplyBean implements Bean{
-	private 
+	private String replyId;
+	private String userId;
+	private String postId;
+	private String reply;
+	private String timestamp;
+
+	public ReplyBean() {}
+
+	public void setReplyId(String rid){
+		replyId = rid;
+	}
+	public void setUserId(String uid){
+		userId = uid;
+	}
+	public void setPostId(String pid){
+		postId = pid;
+	}
+	public void setReply(String rep){
+		reply = rep;
+	}
+	public void setTimestamp(String ts){
+		timestamp = ts;
+	}
+
+	public String getReplyId(){
+		return replyId;
+	}
+	public String getUserId(){
+		return userId;
+	}
+	public String getPostId(){
+		return postId;
+	}
+	public String getReply(){
+		return reply;
+	}
+	public String getTimestamp(){
+		return timestamp;
+	}
 }
--- WEB-INF/jsp/showtest.jsp
+++ WEB-INF/jsp/showtest.jsp
@@ -1,10 +1,13 @@
 <%@ page pageEncoding="Windows-31J" contentType="text/html;charset=Windows-31J" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 	<head>
 		<title>Show Test</title>
 	</head>
 	<body>
-		<img src="${data.imageURL}">
-		<p>${data.caption}</p>
+		<c:forEach var="data" items="${data}">
+			<img src="${data.imageURL}">
+			<h2>${data.caption}</h2>
+		</c:forEach>
 	</body>
 </html>
