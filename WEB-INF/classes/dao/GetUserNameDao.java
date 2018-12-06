084f5afb7a63199725d2f95b7ba7e8c232d43683
Yukiko Mizuno <36286017+ym17304016@users.noreply.github.com>
2018-12-06 15:35:49 +0900

    Add files via upload

---
+++ WEB-INF/classes/dao/GetUserNameDao.java
@@ -0,0 +1,41 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GetProductDao extends AbstractDao{
+    public String getUserName(String uid){
+        Connection cn = null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        String name = null;
+        
+        try{
+            cn = OraConnectionManager.getInstance().getConnection();
+            String sql = "select username from as_user where userid=?";
+            st = cn.prepareStatement(sql);
+            st.setString(1, uid);
+            rs = st.executeQuery();
+            rs.next();
+            name = rs.getString(1);
+        }catch(SQLException e){
+            OraConnectionManager.getInstance().rollback();
+        }finally{
+            try{
+                if(rs!= null){
+                    rs.close();
+                }if(st!= null){
+                    st.close();
+                }
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+        return postList;
+    }
+}
