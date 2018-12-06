38a5b9891f14c1692525bbf49411af4e89106b49
Yukiko Mizuno <36286017+ym17304016@users.noreply.github.com>
2018-12-06 13:53:30 +0900

    Add files via upload

---
+++ WEB-INF/classes/commands/GetPostViewCommand.java
@@ -0,0 +1,23 @@
+package commands;
+
+import java.util.Map;
+import java.util.ArrayList;
+
+import main.ResponseContext;
+import dao.GetPostViewDao;
+import dao.AbstractDaoFactory;
+import dao.OraConnectionManager;
+
+public class GetPostViewCommand extends AbstractCommand{
+    public ResponseContext execute(ResponseContext resc){
+        AbstractDao dao = new GetPostViewDao();
+
+        OraConnectionManager.getInstance().beginTransaction();
+        Object result = dao.getPost();
+        OraConnectionManager.getInstance().closeConnection();
+
+        resc.setResult(result);
+        resc.setTarget("postview");
+        return resc;
+    }
+}
