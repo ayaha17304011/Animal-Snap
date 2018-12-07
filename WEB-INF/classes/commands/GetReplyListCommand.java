c986884c159d98afb824154dbd0092548aea0334
Yukiko Mizuno <36286017+ym17304016@users.noreply.github.com>
2018-12-07 10:49:59 +0900

    Add files via upload

---
+++ WEB-INF/classes/commands/GetReplyListCommand.java
@@ -0,0 +1,24 @@
+package commands;
+
+import java.util.Map;
+import java.util.ArrayList;
+
+import main.ResponseContext;
+import dao.GetReplyListDao;
+import dao.OraConnectionManager;
+import bean.PostBean;
+
+public class GetPostViewCommand extends AbstractCommand{
+    public ResponseContext execute(ResponseContext resc){
+        GetReplyListDao dao = new GetReplyListDao();
+        PostBean pb = new PostBean();
+
+        OraConnectionManager.getInstance().beginTransaction();
+        Object result = dao.getReplyList(pb);
+        OraConnectionManager.getInstance().closeConnection();
+
+        resc.setResult(result);
+        resc.setTarget("postview");
+        return resc;
+    }
+}
