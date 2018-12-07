a5131a12bed7a5cd7f282d2e6c9196b1d2fd79bb
Yukiko Mizuno <36286017+ym17304016@users.noreply.github.com>
2018-12-07 10:11:18 +0900

    Add files via upload

---
+++ WEB-INF/classes/dao/ReplyDao.java
@@ -0,0 +1,40 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import bean.ReplyBean;
+
+public class ReplyDao extends AbstractDao{
+    public void reply(PostBean pb){
+        Connection cn = null;
+        PreparedStatement st = null;
+        ResultSet rs = null;
+
+        try{
+            cn = OraConnectionManager.getInstance().getConnection();
+            String sql = "insert into as_reply values(reply_sequence,?,?,?,sysdate)";
+            st = cn.prepareStatement(sql);
+            st.setInt(1, pb.getUserId());
+            st.setInt(2, pb.getPostId());
+            st.setString(3, pb.getReply());
+            rs = st.executeQuery();
+        }catch(SQLException e){
+            OraConnectionManager.getInstance().rollback();
+        }finally{
+            try{
+                if(rs!= null){
+                    rs.close();
+                }if(st!= null){
+                    st.close();
+                }
+            }catch(SQLException ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+}
