eb65425086fdc2a3bcd51b1a4c935787d0bf147f
Yukiko Mizuno <36286017+ym17304016@users.noreply.github.com>
2018-12-07 11:42:12 +0900

    Add files via upload

---
+++ WEB-INF/classes/commands/GetUserInfoCommand.java
@@ -0,0 +1,23 @@
+package commands;
+
+import java.util.Map;
+import java.util.ArrayList;
+
+import main.ResponseContext;
+import dao.GetReplyListDao;
+import dao.OraConnectionManager;
+import bean.UserBean;
+
+public class GetUserInfoCommand extends AbstractCommand{
+    public ResponseContext execute(ResponseContext resc){
+        GetReplyListDao dao = new GetReplyListDao();
+        UserBean ub = new UserBean();
+
+        OraConnectionManager.getInstance().beginTransaction();
+        Object result = dao.getUserInfo(ub);
+        OraConnectionManager.getInstance().closeConnection();
+
+        resc.setResult(result);
+        return resc;
+    }
+}
