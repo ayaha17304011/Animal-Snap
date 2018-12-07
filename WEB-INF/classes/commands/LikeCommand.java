77eba7ed66c91328739ea51e7d387a207277982a
Yukiko Mizuno <36286017+ym17304016@users.noreply.github.com>
2018-12-07 12:01:46 +0900

    Add files via upload

---
+++ WEB-INF/classes/commands/LikeCommand.java
@@ -0,0 +1,24 @@
+package commands;
+
+import java.util.Map;
+import java.util.ArrayList;
+
+import main.ResponseContext;
+import dao.ReplyDao;
+import dao.OraConnectionManager;
+import bean.PostBean;
+
+public class GetPostViewCommand extends AbstractCommand{
+    public ResponseContext execute(ResponseContext resc){
+        PostBean pb = new PostBean();
+        HttpServletRequest req = resc.getRequest();
+        int userid = req.getParameter("userid");
+
+        OraConnectionManager.getInstance().beginTransaction();
+        dao.like(pb, userid);
+        OraConnectionManager.getInstance().closeConnection();
+
+        resc.setTarget("postview");
+        return resc;
+    }
+}
