4f44d7229869b8f658898261a5c23931741fb261
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2018-12-25 01:58:40 +0900

    fix conflict

--- WEB-INF/classes/commands/AddPostCommand.java
+++ WEB-INF/classes/commands/AddPostCommand.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package commands;
 
 import main.RequestContext;
@@ -17,25 +16,3 @@ public class AddPostCommand extends AbstractCommand{
 		resc.setTarget("showtest");
 		return resc;
 	}
-=======
-package commands;
-
-import dao.PostDB;
-import main.RequestContext;
-import main.ResponseContext;
-import util.Upload;
-import beans.PostBean;
-
-import java.util.ArrayList;
-
-public class AddPostCommand extends AbstractCommand{
-	public ResponseContext execute(ResponseContext resc){
-		RequestContext reqc = getRequestContext();
-		Upload u = new Upload();
-		PostBean pb = u.uploadFlie(reqc);
-		PostDB.addPost(pb);
-		resc.setTarget("timeline");
-		return resc;
-	}
->>>>>>> c5c69c075e9dd4f0e299b7bdcb9668f1a1355ced
-}
--- WEB-INF/classes/commands/AddReplyCommand.java
+++ WEB-INF/classes/commands/AddReplyCommand.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package commands;
 
 import main.ResponseContext;
@@ -18,25 +17,3 @@ public class AddReplyCommand extends AbstractCommand{
 		resc.setTarget("showtest");
 		return resc;
 	}
-=======
-package commands;
-
-import main.ResponseContext;
-import main.RequestContext;
-import beans.ReplyBean;
-import dao.ReplyDB;
-
-public class AddReplyCommand extends AbstractCommand{
-	public ResponseContext execute(ResponseContext resc){
-		RequestContext reqc = getRequestContext();
-		ReplyBean rb = new ReplyBean();
-		String[] reply = reqc.getParameter("reply");
-		String[] pid = reqc.getParameter("postId");
-		rb.setReply(reply[0]);
-		rb.setPostId(pid[0]);
-		ReplyDB.addReply(rb);
-		resc.setTarget("showtest");
-		return resc;
-	}
->>>>>>> c5c69c075e9dd4f0e299b7bdcb9668f1a1355ced
-}
