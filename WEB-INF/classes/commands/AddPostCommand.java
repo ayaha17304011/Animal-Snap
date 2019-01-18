9abc7a8313a2c2e3851c484530d54bdd21b754a2
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-01-18 15:26:04 +0900

    addpost upload update

--- WEB-INF/classes/commands/AddPostCommand.java
+++ WEB-INF/classes/commands/AddPostCommand.java
@@ -9,15 +9,13 @@ import dao.AnimalDao;
 public class AddPostCommand extends AbstractCommand{
 	public ResponseContext execute(ResponseContext resc){
 		RequestContext reqc = getRequestContext();
+		PostBean pb = Upload.uploadFile(reqc);
+		
+		String userId = pb.getUserId();
+		String caption = pb.getCaption();
+		String imageURL = pb.getImageURL();
 
-		String[] uid = (String[])reqc.getParameter("userId");
-		String[] cap = (String[])reqc.getParameter("caption");
-		String[] url = (String[])reqc.getParameter("imageURL");
-		String userId = uid[0];
-		String caption = cap[0];
-		String imageURL = url[0];
-
-		String sql = "INSERT INTO as_post VALUES(as_seq_postId.nextval"+","+userId+"'"+caption+"','"+imageURL+"',sysdate,1)";
+		String sql = "INSERT INTO as_post VALUES(as_seq_postId.nextval,"+userId+"'"+caption+"','"+imageURL+"',sysdate,1)";
 		AnimalDao dao = new AnimalDao();
 		dao.SQLUpdate(sql);
 		
--- WEB-INF/classes/util/Upload.java
+++ WEB-INF/classes/util/Upload.java
@@ -14,21 +14,36 @@ import javax.servlet.ServletException;
 
 @MultipartConfig(fileSizeThreshold=32768,maxFileSize=5242880,maxRequestSize=27262976)
 public class Upload{
-	public PostBean uploadFlie(RequestContext reqc){
+	public static PostBean uploadFile(RequestContext reqc){
 		HttpServletRequest req = (HttpServletRequest)reqc.getRequest();
 
 		String fileName = null;
 		//"docBase" /post-url/
-		String path = req.getServletContext().getRealPath("/WebContent/images/");
+		String path = req.getServletContext().getRealPath("/WebContent/");
 		PostBean post = new PostBean();
 		try{
 			String caption = req.getParameter("caption");
+			String userId = req.getParameter("uid");
 			post.setCaption(caption);
-			Part file = req.getPart("upload");
+			post.setUserId(userId);
+			Part file = req.getPart("post");
+
 			if(req!=null) System.out.println(req.getServletPath());
-			fileName = getFileName(file);
+
+			String ct = file.getContentType();
+			String[] s = ct.split("/");
+			if(s[0] == "image"){
+				path = path + "image/";
+			}else if(s[0] == "video"){
+				path = path + "video";
+			}else{
+				path = path + "etc";
+			}
+
+			fileName = "user" + userId + "_" + getFileName(file);
 			file.write(path+fileName);
 			post.setImageURL(fileName);
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}	catch (ServletException e){
@@ -36,7 +51,7 @@ public class Upload{
 		}
 		return post;
 	}
-	public String getFileName(Part part){
+	public static String getFileName(Part part){
 		String fileName = null;
 		String contentType = part.getContentType();
 		//contentType = FileType / FileFormat => image/png
