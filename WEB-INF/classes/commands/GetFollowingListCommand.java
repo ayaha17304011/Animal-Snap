cc12cc8ba83e7f7457f0a0643f292e63ee83e802
タナカ <35793693+ayaha17304011@users.noreply.github.com>
2019-01-18 10:23:33 +0900

    コンパイル済み、確認未満

---
+++ WEB-INF/classes/commands/GetFollowingListCommand.java
@@ -0,0 +1,26 @@
+package commands;
+
+import java.util.Map;
+import java.util.ArrayList;
+
+import main.ResponseContext;
+import main.RequestContext;
+import dao.OraConnectionManager;
+import beans.FollowBean;
+import dao.AnimalDao;
+
+public class GetFollowingListCommand extends AbstractCommand{
+    public ResponseContext execute(ResponseContext resc){
+        RequestContext reqc = getRequestContext();
+        AnimalDao dao = new AnimalDao();
+        String[] uid = reqc.getParameter("uid");
+        
+        OraConnectionManager.getInstance().beginTransaction();
+        Object result = dao.getFollowinglist(uid[0]);
+        OraConnectionManager.getInstance().closeConnection();
+
+        resc.setResult(result);
+        resc.setTarget("postview");
+        return resc;
+    }
+}
