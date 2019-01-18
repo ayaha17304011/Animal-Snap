88fb2fb5375dca1f992130bb10f94f2ccfeef154
yukiko.m <ym17304016@ga.tera-house.ac.jp>
2019-01-18 10:13:48 +0900

    alter dao PostDao -> AnimalDAO

--- WEB-INF/classes/commands/AddPostCommand.java
+++ WEB-INF/classes/commands/AddPostCommand.java
@@ -4,23 +4,23 @@ import main.RequestContext;
 import main.ResponseContext;
 import util.Upload;
 import beans.PostBean;
-import dao.PostDao;
+import dao.AnimalDao;
 
 import java.util.ArrayList;
 
 public class AddPostCommand extends AbstractCommand{
 	public ResponseContext execute(ResponseContext resc){
 		RequestContext reqc = getRequestContext();
-		// Upload u = new Upload();
+
 		String[] uid = (String[])reqc.getParameter("userId");
 		String[] cap = (String[])reqc.getParameter("caption");
 		String[] url = (String[])reqc.getParameter("imageURL");
 		String userId = uid[0];
 		String caption = cap[0];
 		String imageURL = url[0];
-		// PostBean pb = u.uploadFlie(reqc);
-		String sql = "INSERT INTO as_post VALUES(as_seq_postId.nextval"+","+userId+"'"+caption+"','"+imageURL+"',sysdate,1);"
-		PostDao dao = new PostDao();
+
+		String sql = "INSERT INTO as_post VALUES(as_seq_postId.nextval"+","+userId+"'"+caption+"','"+imageURL+"',sysdate,1)";
+		AnimalDao dao = new AnimalDao();
 		dao.SQLUpdate(sql);
 		
 		resc.setTarget("postview"); //ì]ëóêÊurlÇÕï ìrïœçX
