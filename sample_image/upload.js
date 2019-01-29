890b640c74f804a56856acbf2459c1da98cb16c7
タナカ <35793693+ayaha17304011@users.noreply.github.com>
2019-01-29 14:26:14 +0900

    Add files via upload

---
+++ sample_image/upload.js
@@ -0,0 +1,16 @@
+$('#file_photo').change(
+    function () {
+        if (!this.files.length) {
+            return;
+        }
+
+        var file = $(this).prop('files')[0];
+        var fr = new FileReader();
+        $('.preview').css('background-image', 'none');
+        fr.onload = function() {
+            $('.preview').css('background-image', 'url(' + fr.result + ')');
+        }
+        fr.readAsDataURL(file);
+        $(".preview img").css('opacity', 0);
+    }
+);
