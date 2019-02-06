3fb9fe003a8eda7a72c450e79980192509733d8d
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2019-02-06 13:50:06 +0900

    multiple upload

--- WEB-INF/classes/util/Upload.java
+++ WEB-INF/classes/util/Upload.java
@@ -1,3 +1,10 @@
+/** 0205
+*file name not fix yet
+*when upload more than 1 img or vid
+*uploading file save as same name insame diractory
+*cause file overwrite  
+**/
+
 package util;
 
 import main.RequestContext;
@@ -5,6 +12,8 @@ import beans.PostBean;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.Collection;
+import java.util.Iterator;
 import java.text.SimpleDateFormat;
 
 import javax.servlet.http.Part;
@@ -18,6 +27,7 @@ public class Upload{
 		HttpServletRequest req = (HttpServletRequest)reqc.getRequest();
 
 		String fileName = null;
+		String allFileName = "";
 		//"docBase" /post-url/
 		String path = req.getServletContext().getRealPath("/WebContent/");
 		PostBean post = new PostBean();
@@ -27,27 +37,36 @@ public class Upload{
 			post.setCaption(caption);
 			post.setUserId(userId);
 
-			//incomplete
-			List<Part> fileParts = req.getParts().stream().filter(part -> "file".equals(part.getName())).collect(Collectors.toList());
-
-			if(req!=null) System.out.println(req.getServletPath());
-
 			Date d = new Date();
-			SimpleDateFormat d1 = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+			SimpleDateFormat d1 = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
 			String date = d1.format(d);
 
-			String[] contentType = ((String)file.getContentType()).split("/");
-			//contentType = FileType / FileFormat => image/png
-			//s[0] = image, s[1] = png
-			if(contentType[0].equals("image")){
-				fileName = "images/user" + userId + "_" + date + ".jpg";
-			}else if(contentType[0].equals("video")){
-				fileName = "videos/user" + userId + "_" + date + ".mp4";
+			Collection<Part> part = req.getParts();
+			Iterator it = part.iterator();
+			int i = 0;
+			while(it.hasNext()){
+				
+				Part file = (Part)it.next();
+				System.out.println(file.getName());
+				if(file.getName().equals("post")){
+					System.out.println("File = "+file);
+					String[] contentType = file.getContentType().split("/");
+					System.out.println("type = "+contentType[0]);
+					//contentType = FileType / FileFormat => image/png
+					//s[0] = image, s[1] = png
+					if(contentType[0].equals("image")){
+						fileName = "images/user" + userId + "_" + i + "_" + date + ".jpg";
+					}else if(contentType[0].equals("video")){
+						fileName = "videos/user" + userId + "_" + i + "_" + date + ".mp4";
+					}
+				i++;
+				file.write(path+fileName);
+				allFileName += "," + fileName;
+				}
 			}
-			System.out.println(fileName);
-			post.setImageURL(fileName);
-			file.write(path+fileName);
-
+			String URL = allFileName.substring(1);
+			post.setImageURL(URL);
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}	catch (ServletException e){
